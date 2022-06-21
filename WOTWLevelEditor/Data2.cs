603876e5444c2e6b95fd8d2994c122797707700b
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2022-06-21 12:52:53 -0400

    initial work on second data set

---
+++ WOTWLevelEditor/Data2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOTWLevelEditor
+{
+    public class Data2 // Figuring out what this stuff does comes later
+    {
+        public int ID { get; }
+
+        public Data2(byte[] pattern)
+        {
+            ID = BitConverter.ToInt32(pattern, 0);
+        }
+
+        public override string ToString()
+        {
+            return ID.ToString();
+        }
+    }
+}
--- WOTWLevelEditor/Program.cs
+++ WOTWLevelEditor/Program.cs
@@ -16,7 +16,7 @@ namespace WOTWLevelEditor
 
             int parserLocation = 0;
 
-            parserLocation += 4; // unknown bytes
+            parserLocation += 4; // Unknown bytes
 
             byte[] fileLengthBytes = ByteHelper.GetAtIndex(bytes, 0x4, 4);
             fileLengthBytes = fileLengthBytes.Reverse().ToArray();
@@ -24,18 +24,19 @@ namespace WOTWLevelEditor
             Console.WriteLine("File Length: " + FileLength + " bytes");
             parserLocation += 4;
 
-            Debug.Assert(Enumerable.SequenceEqual(ByteHelper.GetAtIndex(bytes, parserLocation, 4), new byte[] { 0x00, 0x00, 0x00, 0x11 })); // always equals 00-00-00-11
+            Debug.Assert(Enumerable.SequenceEqual(ByteHelper.GetAtIndex(bytes, parserLocation, 4), new byte[] { 0x00, 0x00, 0x00, 0x11 })); // Always equals 00-00-00-11
             parserLocation += 4;
 
-            parserLocation += 4; // unknown bytes
+            parserLocation += 4; // Unknown bytes
 
-            Debug.Assert(Enumerable.SequenceEqual(ByteHelper.GetAtIndex(bytes, parserLocation, 16), System.Text.Encoding.ASCII.GetBytes("\0\0\0\02018.4.24f1\0"))); // always equals "\0\0\0\02018.4.24f1\0"
+            Debug.Assert(Enumerable.SequenceEqual(ByteHelper.GetAtIndex(bytes, parserLocation, 16), System.Text.Encoding.ASCII.GetBytes("\0\0\0\02018.4.24f1\0"))); // Always equals "\0\0\0\02018.4.24f1\0"
             parserLocation += 16;
 
-            Debug.Assert(Enumerable.SequenceEqual(ByteHelper.GetAtIndex(bytes, parserLocation, 5), new byte[] { 0x13, 0x00, 0x00, 0x00, 0x00 })); // always equals 13-00-00-00-00
+            Debug.Assert(Enumerable.SequenceEqual(ByteHelper.GetAtIndex(bytes, parserLocation, 5), new byte[] { 0x13, 0x00, 0x00, 0x00, 0x00 })); // Always equals 13-00-00-00-00
             parserLocation += 5;
 
-            ObjectType[] objectTypes = new ObjectType[BitConverter.ToInt32(ByteHelper.GetAtIndex(bytes, parserLocation, 4))];
+            // Get list of object types
+            ObjectType[] objectTypes = new ObjectType[BitConverter.ToInt32(bytes, parserLocation)];
             parserLocation += 4;
             
             for (int i = 0; i < objectTypes.Length; i++)
@@ -52,10 +53,29 @@ namespace WOTWLevelEditor
                 }
             }
 
+            Data2[] data2s = new Data2[BitConverter.ToInt32(bytes, parserLocation)];
+            parserLocation += 4;
 
+            // Return to multiple of 4
+            while (parserLocation % 4 != 0)
+            {
+                parserLocation++;
+            }
+
+            for (int i = 0; i < data2s.Length; i++)
+            {
+                data2s[i] = new Data2(ByteHelper.GetAtIndex(bytes, parserLocation, 20));
+                parserLocation += 20;
+            }
 
             Console.WriteLine("Object types (" + objectTypes.Length + "):");
             foreach(ObjectType i in objectTypes)
+            {
+                Console.WriteLine("    " + i.ToString());
+            }
+
+            Console.WriteLine("Data2 (" + data2s.Length + "):");
+            foreach (Data2 i in data2s)
             {
                 Console.WriteLine("    " + i.ToString());
             }
