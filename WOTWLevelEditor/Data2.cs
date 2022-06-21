86f73aae574a7af0639db91e7f2ab939b80cadae
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2022-06-21 13:08:02 -0400

    just gonna leave these here

--- WOTWLevelEditor/Data2.cs
+++ WOTWLevelEditor/Data2.cs
@@ -5,16 +5,22 @@ namespace WOTWLevelEditor
     public class Data2 // Figuring out what this stuff does comes later
     {
         public int ID { get; }
+        public int Data3 { get; }
+        public int Data4 { get; }
+        public int Data5 { get; }
 
         public Data2(byte[] pattern)
         {
             ID = BitConverter.ToInt32(pattern, 0);
             Debug.Assert(BitConverter.ToInt32(pattern, 4) == 0); // Always 0 for some reason
+            Data3 = BitConverter.ToInt32(pattern, 8);
+            Data4 = BitConverter.ToInt32(pattern, 12);
+            Data5 = BitConverter.ToInt32(pattern, 16);
         }
 
         public override string ToString()
         {
-            return ID.ToString();
+            return BitConverter.ToString(BitConverter.GetBytes(ID)) + ", " + BitConverter.ToString(BitConverter.GetBytes(Data3)) + ", " + BitConverter.ToString(BitConverter.GetBytes(Data4)) + ", " + BitConverter.ToString(BitConverter.GetBytes(Data5));
         }
     }
 }
