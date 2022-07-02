be950c73d45c5f0bd3bbbf6ce01e5280bb663554
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2022-07-02 19:02:47 -0400

    allow setting rotation

--- WOTWLevelEditor/Objects/Transform.cs
+++ WOTWLevelEditor/Objects/Transform.cs
@@ -6,7 +6,7 @@ namespace WOTWLevelEditor.Objects
     public class Transform : UnityObject
     {
         public ObjectID GameObjectID => (ObjectID)parameters[0];
-        public Quaternion Rotation => (Quaternion)parameters[1];
+        public Quaternion Rotation { get => (Quaternion)parameters[1]; set => parameters[1] = value; }
         public Vector3 Position { get => (Vector3)parameters[2]; set => parameters[2] = value; }
         public Vector3 Scale { get => (Vector3)parameters[3]; set => parameters[3] = value; }
         public List<ObjectID> ChildrenIDs => (List<ObjectID>)parameters[4];
