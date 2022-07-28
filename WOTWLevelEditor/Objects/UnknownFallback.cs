d6ba5989eb4a687c8641f802f191a40538d4f6cb
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2022-07-28 17:20:49 -0400

    implement Get/ConvertReferences for UnknownFallback to clarify object type

--- WOTWLevelEditor/Objects/UnknownFallback.cs
+++ WOTWLevelEditor/Objects/UnknownFallback.cs
@@ -1,4 +1,6 @@
-﻿namespace WOTWLevelEditor.Objects
+﻿using System.Diagnostics;
+
+namespace WOTWLevelEditor.Objects
 {
     /// <summary>
     /// Stores the raw data of unparsed objects.
@@ -9,6 +11,17 @@
         public UnknownFallback(Level level, ObjectType type, int id, byte[] data) : base(level, type, id, Array.Empty<object>())
         {
             Data = data;
+        }
+
+        public override List<ObjectID> GetReferences()
+        {
+            Debug.WriteLine(ThisType.ToString() + " does not support " + nameof(GetReferences));
+            return new();
+        }
+
+        public override void ConvertReferences(Dictionary<ObjectID, ObjectID> conversionTable)
+        {
+            Debug.WriteLine(ThisType.ToString() + " does not support " + nameof(ConvertReferences));
         }
 
         public override string ToString()
